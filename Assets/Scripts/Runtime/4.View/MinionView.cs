56b83c8289ee0d5f9d9e7c139a87ecfabc9423c0
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-04-24 09:15:55 +0900

    [add]僕はこいつを殺します

--- Assets/Scripts/Runtime/4.View/MinionView.cs
+++ Assets/Scripts/Runtime/4.View/MinionView.cs
@@ -1,25 +1,58 @@
-using Domain;
+using Adaptor;
 using UnityEngine;
 
 namespace View
 {
     public class MinionView : MonoBehaviour
     {
-        public MinionType MinionType => _minionType;
-        public int ID => GetID();
-        public void Initialize(MinionViewModel viewModel)
+        public int ID => _id;
+        public void Initialize(AttackController attackController,
+                               AttackPresenter attackPresenter,
+                               KillController killController,
+                               KillPresenter killPresenter)
         {
-            _viewModel = viewModel;
+            _attackController = attackController;
+            _attackPresenter = attackPresenter;
 
-            _viewModel.OnMinionHealthChanged += MinionTakeDamageHandler;
+
+            _killController = killController;
+            _killPresenter = killPresenter;
+
+            _id = GetID();
+
+        }
+        public void TakeDamage(int attackerId)
+        {
+            _attackController.Attack(attackerId, _id);
+            ApplyDamage();
+        }
+        private AttackController _attackController;
+        private AttackPresenter _attackPresenter;
+
+        private KillController _killController;
+        private KillPresenter _killPresenter;
+
+        private int _id;
+        private float _currentHealth;
+        private int GetID() => gameObject.GetInstanceID();
+        private void ApplyDamage()
+        {
+            _currentHealth = _attackPresenter.Present(_id);
+            if (_killPresenter.Present(_id) && _currentHealth <= 0)
+            {
+                _killController.Kill(_id);
+                Destroy(gameObject);
+            }
         }
 
-        [SerializeField, Tooltip("ミニオンのタイプ")] private MinionType _minionType;
-        private MinionViewModel _viewModel;
-        private int GetID() => gameObject.GetInstanceID();
-        private void MinionTakeDamageHandler(float damage)
+        private void OnTriggerEnter(Collider other)
         {
+            if (_attackPresenter != null) return;
 
+            if (other.TryGetComponent<MinionView>(out var minionView))
+            {
+                minionView.TakeDamage(_id);
+            }
         }
     }
 }
